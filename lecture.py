e38a43baf997615e580c123b78a798b29f2c8506
miguel12344 <ronalmiguel2005@gmail.com>
2024-05-08 08:11:59 -0500

    añadi un archivo lecture a mi carpeta

---
+++ lecture.py
@@ -0,0 +1,15 @@
+x = 5
+y = 10
+z = 15
+
+# Usando operador 'and'
+if x < y and y < z:
+    print("x es menor que y y y es menor que z")
+
+# Usando operador 'or'
+if x == y or x == z:
+    print("x es igual a y o igual a z")
+
+# Usando operador 'not'
+if not (x > y):
+    print("x no es mayor que y")
